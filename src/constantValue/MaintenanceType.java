58a2a68f6896b512bafed2195387c5e93feaa676
Anne Zhao <xzhao16@luc.edu>
2016-02-13 12:14:22 -0600

    testing merge

---
+++ src/constantValue/MaintenanceType.java
@@ -0,0 +1,7 @@
+package constantValue;
+
+public class MaintenanceType {
+	public final String LIGHTENING = "Lightening";
+	public final String HVAC = "H.V.A.C.";
+	public final String PLUMBING = "Plumbing";
+}
---
+++ src/constantValue/UsageType.java
@@ -0,0 +1,8 @@
+package constantValue;
+
+public class UsageType {
+	public final String RESIDENTIAL = "Residential Property";
+	public final String RETAIL = "Retail Property";
+	public final String OFFICE = "Offce Property";
+	public final String INDUSTRIAL = "Industrial Property";
+}
