f9803161ad447f0cecea638166af416e22ed1dca
emishparii/em <emishparii@icstars.org/em>
2016-02-22 00:46:02 -0600

    Implementation of facilityMaintenance (few methods still need to be implemented)

--- src/model/facilityMaintenance/MaintenanceLog.java
+++ src/model/facilityMaintenance/MaintenanceLog.java
@@ -1,35 +1,63 @@
 package model.facilityMaintenance;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MaintenanceLog {
-    private Request request;
-    
+    //private Request request;
+	private DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
     private List<Request> requests;
     
     public MaintenanceLog(Request ... request) {
     	setRequests(Arrays.asList(request));
     }
 
+    //returns a list of requests with not scheduled maintenances
+    //and prints to console information about them
     public Object listFacilityProblems() {
-        // TODO
-        return null;
+    	List<Request> problems = new ArrayList<>();
+        for (Request r : requests) {
+        	if (r.getMaintenance().getStatus().equals("pending")) {
+        		problems.add(r);
+        		System.out.println("Request " + r.getRequestID() + " : " + r.getTextDescription() + 
+        				" was made on " + df.format(r.getDateRequested()));
+        	}
+        }
+        return problems;
+    }
+    
+    //creates a new Request object
+    //and returns it back
+    public  Request makeFacilityMaintenanceRequest() {
+        Request r = new Request();
+        requests.add(r);
+        return r;
     }
 
-    public  Object makeFacilityMaintenanceRequest() {
-        // TODO
-        return null;
+    //prints to console info about all requests for maintenance
+    //and returns the list of them
+    public List<Request> listMaintRequest(){
+    	for (Request r : requests) {
+        	System.out.println("Request " + r.getRequestID() + " : " + r.getTextDescription() + 
+        				" was made on " + df.format(r.getDateRequested()));
+        }
+        return requests;
     }
 
-    public Object listMaintRequest(){
-        // TODO
-        return null;
-    }
-
-    public Object listMaintenance() {
-        // TODO
-        return null;
+    //prints to console info about all maintenances of the facility
+    //and returns the list of them
+    public List<Maintenance> listMaintenance() {
+    	List<Maintenance> maintenances = new ArrayList<>();
+    	for (Request r : requests) {
+    		Maintenance m = r.getMaintenance();
+    		maintenances.add(m);
+        	System.out.println("Maintenance " + m.getMaintenanceID() + " of " + m.getMaintenanceType() + " type " +
+        				" is currently in " + m.getStatus() + " status.");
+        }
+        return maintenances;
     }
 
     public Object calcDowntime() {
@@ -42,14 +70,6 @@ public class MaintenanceLog {
         return null;
     }
 
-	public Request getRequest() {
-		return request;
-	}
-
-	public void setRequest(Request request) {
-		this.request = request;
-	}
-
 	public List<Request> getRequests() {
 		return requests;
 	}
@@ -57,4 +77,12 @@ public class MaintenanceLog {
 	public void setRequests(List<Request> requests) {
 		this.requests = requests;
 	}
+	
+//	public Request getRequest() {
+//	return request;
+//}
+//
+//public void setRequest(Request request) {
+//	this.request = request;
+//}
 }
